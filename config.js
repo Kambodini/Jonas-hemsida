34d1ffa99abd0a48734a117f5f9c601a254485c3
Kambodini <karl.tornered@nyamunken.se>
2026-03-17 12:17:25 +0100

    Add Firebase and Football API configuration

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase konfiguration
+export const firebaseConfig = {
+    apiKey: "AIzaSyAwhpoNdvc9tbc5Ee0owBApxiXW4X8-HS0",
+    authDomain: "jonas-hemsida.firebaseapp.com",
+    projectId: "jonas-hemsida",
+    storageBucket: "jonas-hemsida.firebasestorage.app",
+    messagingSenderId: "65137999270",
+    appId: "1:65137999270:web:f76cc4968956cc4e7f6367",
+    measurementId: "G-P68P0EXRJV"
+};
+
+// API Nycklar
+export const FOOTBALL_API_KEY = '362dbf4c4cbd4a4dab2fad63440d0000';
